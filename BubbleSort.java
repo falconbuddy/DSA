7ea0ffffbce519a729f2e92b7c512728dd4ee7a3
falconbuddy <pasindurajapaksha2468@gmail.com>
2025-02-15 19:39:11 +0530

    Add files via upload

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,53 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-package Desktop.BubbleSort;
-
-import java.util.Arrays;
-
+package Sorting;
+import java.util.Scanner;
 public class BubbleSort {
-  
-    public void swap(Integer []a,int i,int j){
-    //swap is a method
-     int temp;
-     temp = a[i];
-     a[i]=a[j];
-     a[j]=temp; 
-    }
-       
-    public  void bubbleSort(Integer[] a) {
-        int num = a.length;
-        for(int i=0;i<num;i++){
-            for(int j=0; j<num -i-1; j++){
-                if(a[j]> a[j+1]){
-                        swap(a,j,j+1);
-            }
-                //after each inner loop
-                System.out.println( "i = " +(i+1) + "; j = " +(j+1) +";" +Arrays.deepToString(a));
-            }
-            }
-        }
-        public static void printArray(Integer[]a){
-            for(int n:a){
-                System.out.print(n+" ");
-            }
-            System.out.println();
-        }
-        
-        public static void main(String[]args){
-            Integer[]numbers={ 76 , 6 , 107 , 92 , 21 , 23 , 5 , 9 , 18 , 8143};
-            
-                   System.out.println("Before Sort the Array :");
-                   printArray(numbers);
-                   
-                   BubbleSort sorter= new BubbleSort();
-                   sorter.bubbleSort(numbers);
-                   
-                   System.out.println("After sort the Array :");
-                   printArray(numbers);
-        }
 
-    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    Scanner scanner = new Scanner(System.in);
+
+		    System.out.print("Enter the number of elements to sort: ");
+		    int n = scanner.nextInt();
+
+		    int[] arr = new int[n];
+
+		    System.out.println("Enter the elements: ");
+		    for (int i = 0; i < n; i++) {
+		      arr[i] = scanner.nextInt();
+		    }
+
+		    // Perform modified bubble sort
+		    modifiedBubbleSort(arr);
+
+		    System.out.println("Modified Bubble Sorted array: ");
+		    for (int i : arr) {
+		      System.out.print(i + " ");
+		    }
+		    System.out.println();
+		  }
+
+		  public static void BubbleSort(int[] arr) {
+		    int n = arr.length;
+		    boolean swapped = true; // Flag to track if any swaps occurred
+
+		    for (int i = 0; i < n - 1 && swapped; i++) {
+		      swapped = false; // Reset flag for each pass
+
+		      for (int j = 0; j < n - i - 1; j++) {
+		        if (arr[j] > arr[j + 1]) {
+		          int temp = arr[j];
+		          arr[j] = arr[j + 1];
+		          arr[j + 1] = temp;
+		          swapped = true; // Set flag if a swap occurred
+		        }
+		      }
+		    }
+	}
+
 }
- 
--- PriorityQueue1.java
+++ PriorityQueue1.java
@@ -2,26 +2,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package Desktop.PriorityQ;
 
-import java.util.PriorityQueue;
+/**
+ *
+ * @author Asus
+ */
+import java.util.PriorityQueue ;
 public class PriorityQueue1 {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
-        PriorityQueue<Integer> pQueue=new PriorityQueue<Integer>();
-        
-        pQueue.add(20);
-        pQueue.add(10);
-        pQueue.add(15);
-        
-        System.out.println(pQueue);
-        
-        int size = pQueue.size();
-        System.out.println("Size : "+size);
-        
-        for(int i = 0; i<size; i++)
-        {
-            System.out.println(pQueue.poll());
-        }
+        // TODO code application logic here
+        	PriorityQueue <Integer> pQueue = new PriorityQueue <Integer>();
+			
+		pQueue.add(20);
+		pQueue.add(10);
+		pQueue.add(15);
+		
+		System.out.println(pQueue);
+		
+		int size = pQueue.size();
+		System.out.println("Size : " +pQueue);
+	   
+	    for (int i=0; i<size;i++){
+			System.out.println(pQueue.poll());
+		}
     }
 }
